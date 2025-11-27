7f004dc6b53ec8b514d4a47f9d1c68c8052352ee
VadimLazutin <Tyuiu.LazutinVS@outlook.com>
2025-11-27 14:16:28 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.LazutinVS.Sprint5.Task0.V25.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.LazutinVS.Sprint5.Task0.V25.Lib
+{
+    public class DataService
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask0.txt");
+            double y = Math.Round((3 * Math.Pow(x, 4) + 1) / Math.Pow(x, 3), 3);
+            File.WriteAllText(path, y.ToString());
+            return path;
+
+        }
+    }
+}
---
+++ Tyuiu.LazutinVS.Sprint5.Task0.V25/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.LazutinVS.Sprint5.Task0.V25.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int x = 3;
+        DataService ds = new DataService();
+
+        Console.WriteLine("Значение выражения: " + File.ReadAllText(ds.SaveToFileTextData(x)));
+    }
+}
