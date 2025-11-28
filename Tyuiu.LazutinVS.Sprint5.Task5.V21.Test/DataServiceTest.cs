5febfcee79e63d891842fea9fa38eaebf82ab609
VadimLazutin <Tyuiu.LazutinVS@outlook.com>
2025-11-28 21:49:10 +0500

    Task5

---
+++ Tyuiu.LazutinVS.Sprint5.Task5.V21.Test/DataServiceTest.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.LazutinVS.Sprint5.Task5.V21.Lib;
+
+namespace Tyuiu.LazutinVS.Sprint5.Task5.V21.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.LazutinVS.Sprint5.Task5.V21/Program.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.LazutinVS.Sprint5.Task5.V21.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        string path = @"C:\DataSprint5\InPutDataFileTask5V21.txt";
+        using (StreamReader reader = new StreamReader(path))
+        {
+            string? line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+            }
+
+        }
+        double res = ds.LoadFromDataFile(path);
+        Console.WriteLine(res);
+    }
+}
